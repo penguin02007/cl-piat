1f84fd592e696ecc4a3d79875748b711c5a9fec6
Leo Chan <ka.chan@rakuen.com>
2020-03-16 23:54:22 -0400

    set http proxy for git

--- vx-simulation/helper_scripts/auto_mgmt_network/OOB_Server_Config_auto_mgmt.sh
+++ vx-simulation/helper_scripts/auto_mgmt_network/OOB_Server_Config_auto_mgmt.sh
@@ -218,6 +218,7 @@ cat <<EOT >> /home/cumulus/.gitconfig
 EOT
 
 sudo su - cumulus -c '\
+git config --global http.proxy http://pkg.proxy.prod.jp.local:10080
 git clone https://github.com/CumulusNetworks/cl-piat.git;
 ansible-playbook /home/cumulus/cl-piat/vx-simulation/helper_scripts/install-runner-live.yaml;\
 '
