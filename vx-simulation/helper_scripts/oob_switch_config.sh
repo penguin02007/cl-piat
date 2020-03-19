fa8e42c5e485db72fef11c95a10a62caed71b421
Leo Chan <ka.chan@rakuen.com>
2020-03-19 17:46:54 -0400

    add cumulus passwordless and autoogin to oob switch

--- vx-simulation/helper_scripts/oob_switch_config.sh
+++ vx-simulation/helper_scripts/oob_switch_config.sh
@@ -18,7 +18,12 @@ iface vagrant inet dhcp
 
 source /etc/network/interfaces.d/*
 EOT
+echo " ###Making cumulus passwordless sudo ###"
+echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
 
+echo " ###Auto login cumulus user ###"
+echo "sudo su - cumulus" >> /home/vagrant/.bash_profile
+echo "exit" >> /home/vagrant/.bash_profile
 #/usr/share/doc/ifupdown2/examples/generate_interfaces.py -b | grep -v "#" >> /etc/network/interfaces.d/bridge
 
 #sed -i 's/vagrant//g' /etc/network/interfaces.d/bridge
